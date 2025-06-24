759f0d6deb8c4236b9bdc285656f5467c7d4da62
Emilse Arias <emilsearias@gmail.com>
2025-06-23 21:36:58 -0300

    agregamos el logo de los simpsons en nuestro proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Container, Button } from "react-bootstrap";
 import Frase from "./components/Frase";
+import logo from './assets/logosimpson.png'
 
 function App() {
   //  https://thesimpsonsquoteapi.glitch.me/quotes
   return (
     <>
       <Container className="text-center my-5">
-        <img src="" alt="Logo simpson" className="w-50 mb-4" />
+        <img src={logo} alt="Logo simpson" className="w-50 mb-4" />
 
         {/* <div>
           <Spinner animation="grow" variant="info" />
